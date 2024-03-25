c39f7b40e09e889fdb97bd0874fac1aa85f7f553
Alevtina Sergienko <sergienkoalevtina@gmail.com>
2024-03-25 20:32:55 +0300

    practice form tests fix

--- src/test/java/guru/qa/PracticeFormTest.java
+++ src/test/java/guru/qa/PracticeFormTest.java
@@ -1,8 +1,10 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.selector.ByText;
 import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -16,9 +18,14 @@ public class PracticeFormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy ="eager";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
 
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
     }
 
     @Test
@@ -31,23 +38,24 @@ public class PracticeFormTest {
         $("#userNumber").setValue("7911111111"); //ввести номер телефона
         $("#dateOfBirthInput").click(); //клик в поле даты рождения
         $(".react-datepicker__month-select").click(); //клик в список выбора месяца
-        $(byText("May")).click(); //выбор месяца
+        $(by("value", "4")).click(); //выбор месяца (исправлено)
         $(".react-datepicker__year-select").click(); // клик в список выбора года
-        $(byText("1997")).click(); //выбор года
+        $(by("value", "1997")).click(); //выбор года (исправлено)
         $(".react-datepicker__day--023").click(); //выбор дня
-        $(".css-2b097c-container input").setValue("computer science").pressEnter(); //выбор предмета
+        $("#subjectsInput").setValue("computer science").pressEnter(); //выбор предмета
         $("#hobbiesWrapper").$(byText("Music")).click(); //выбор чекбокса
         $("#uploadPicture").uploadFromClasspath("26.jpg"); //загрузка картинки
         $("#currentAddress-wrapper").$("#currentAddress").setValue("Moscow, Pushkin street, Kolotushkin house"); //ввод адреса
         $("#state").click(); //клик в поле ввода штата
-        $(byText("NCR")).click(); //выбор штата
+        $("#react-select-3-option-0").click(); //выбор штата (исправлено)
         $("#city").click(); //клик в поле ввода города
-        $(byText("Delhi")).click(); //выбор города
+        $("#react-select-4-option-0").click(); //выбор города (исправлено)
         $("#submit").click(); //клик на кнопку подтверждения
 
         //проверки
         $(".modal-body").shouldHave(text("Alevtina Sergienko"));
         $(".modal-body").shouldHave(text("Alina230597@mail.ru"));
+        $(".modal-body").shouldHave(text("7911111111")); //(исправлено)
         $(".modal-body").shouldHave(text("Female"));
         $(".modal-body").shouldHave(text("23 May,1997"));
         $(".modal-body").shouldHave(text("Computer Science"));
